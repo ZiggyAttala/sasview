3f394105ff934293849112f5a6eee41bebd1ba42
ZiggyAttala <ziggy.attala@stfc.ac.uk>
2019-06-28 13:52:39 +0100

    experimenting with ways to rewrite inverter in python

---
+++ src/sas/sascalc/pr/py_invertor.py
@@ -0,0 +1,24 @@
+
+"""
+Implementing C invertor functions in Python
+"""
+
+def ortho_transformed_smeared_py(d_max, n, height, width, q, npts):
+    y = 0
+    z = 0
+    sum = 0
+
+    i, j, n_height, n_width
+    count_w
+    fnpts
+    sum = 0.0
+    fnpts = float(npts-1.0)
+
+    n_height = [npts, 1](height>0)
+    n_width = [npts, 1](width>0)
+    print(n_height)
+    print(n_width)
+
+ortho_transformed_smeared_py(0, 0, 3, 3, 0, 5)
+
+
